00ee9739ce51e5b1968470d218c6057c8b6b6f2e
betferna <betferna@student.codam.nl>
2026-04-22 12:49:26 +0200

    changes in revrot

--- push_swap.h
+++ push_swap.h
@@ -49,7 +49,8 @@ int		ft_isdigit(int c);
 void	ft_lstadd_front(t_stack *lst, t_stack *new);
 void	ft_delone(t_stack *lst);
 void 	ft_push(t_stack *dst, t_stack *src);
-
+void	ft_revrot(t_stack **stack);
+void 	ft_stack_add_front(t_stack **stack, t_stack *tail);
 
 
 
--- src/ft_revrot.c
+++ src/ft_revrot.c
@@ -1,34 +1,40 @@
-// #include "push_swap.h"
+#include "push_swap.h"
 
-// //move the tail to the front:
-// static void	revrot(t_stack **stack)
-// {
-// 	t_stack *tail;
+//move the tail to the front:
+void	ft_revrot(t_stack **stack)
+{
+	t_stack *tail;
+	t_stack *new_tail;
 
-// 	if (!stack || !*stack || !(*stack)->next)
-// 		return;
-// 	tail = *stack;
-// 	while (tail -> next != NULL)
-// 		tail = tail->next;
-// 	tail->prev->next = NULL;
-// 	ft_stack_add_front(stack, tail);
-// }
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+	tail = *stack;
+	while (tail -> next != NULL)
+	{
+		if (tail->next->next == NULL)
+			new_tail = tail;
+		tail = tail->next;
+	}
+	ft_stack_add_front(stack, tail);
+	new_tail->next = NULL;
+}
 
-// void ft_rra(t_stack **a)
-// {
-// 	revrot(a);
-// 	write (1, "rra\n", 4);
-// }
+void ft_rra(t_stack **a)
+{
+	ft_revrot(a);
+	write (1, "rra\n", 4);
+}
 
-// void ft_rrb(t_stack **b)
-// {
-// 	revrot(b);
-// 	write (1, "rrb\n", 4);
-// }
+void ft_rrb(t_stack **b)
+{
+	ft_revrot(b);
+	write (1, "rrb\n", 4);
+}
 
-// void ft_rrr(t_stack **a, t_stack **b)
-// {
-// 	revrot(a);
-// 	revrot(b);
-// 	write (1, "rrr\n", 4);
-// }
+void ft_rrr(t_stack **a, t_stack **b)
+{
+	ft_revrot(a);
+	ft_revrot(b);
+	write (1, "rrr\n", 4);
+}
+
