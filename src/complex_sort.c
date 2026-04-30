f850ba32e2db266b0d7ccf7cfd716d73f3b1bbf2
maayanarad-lang <maayan@macbookpro.home>
2026-04-30 22:23:25 +0200

    update

--- src/complex_sort.c
+++ src/complex_sort.c
@@ -2,5 +2,19 @@
 
 void complex_sort(t_stack **a, t_stack **b)
 {
+    (*a)->index = 0;
+    int n;
+    int max_bits;
+    int bit_position;
+    
+    n = 0;
+    bit_position = 0;
+    while (*a != NULL)
+    {
+        (*a)->next->index += 1;
+        n++;
+    }
+    max_bits = n-1;
+    while 
 
 }
