81612927445b36b16ed3598ba52c45d47c8b5eb4
Maayan Arad <marad@f0r1s2.codam.nl>
2026-05-04 10:11:23 +0200

    update

--- src/complex_sort.c
+++ src/complex_sort.c
@@ -2,13 +2,15 @@
 
 void complex_sort(t_stack **a, t_stack **b)
 {
-    (*a)->index = 0;
-    int n;
-    int max_bits;
+    int n;//#numbers
+    int max_bits;// n-1 the total number or numbers in the stack. 
     int bit_position;
+    int i;
     
     n = 0;
+    i = 0;
     bit_position = 0;
+    (*a)->index = 0;
     while (*a != NULL)
     {
         (*a)->next->index += 1;
@@ -17,4 +19,28 @@ void complex_sort(t_stack **a, t_stack **b)
     max_bits = n-1;
     while ()
 
+}
+
+void assign_index(t_stack **a)
+{
+    int count;
+    t_stack *node_i;
+    t_stack *node_j;
+
+    if (!a || !*a)
+        return ;
+    node_i = *a;
+    while (node_i != NULL)
+    {
+        node_j = *a;
+        count = 0;
+        while (node_j != NULL)
+        {
+            if (node_j->value < node_i->value)
+                count++; 
+            node_j = node_j->next;
+        }
+    node_i->index = count;
+    node_i = node_i->next;
+    }
 }
